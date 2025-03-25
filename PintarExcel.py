3cfa46cf048ef2b3883115450650e9bfead9639f
Evans <egaibor812@gmail.com>
2025-03-25 16:06:36 -0500

    Pintando Excel, hay que optimizar como poner más columnas

--- PintarExcel.py
+++ PintarExcel.py
@@ -47,7 +47,8 @@ wb = Workbook()
 ws = wb.active
 
 # Aplicar color a la celda B2
-abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+abecedario = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL"]
 columna = "B"
 fila = 1
 color = "000000"
